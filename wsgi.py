7d01081aeada7f09f25b26ea36e4c4f06e93f575
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2024-03-28 06:31:24 +0300

    fix server issues

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 from app import create_app
 
+# Create Flask application
+app = create_app()
 
-
+# This is required for Gunicorn to work correctly
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
